bde49830f6c190edcd113544e386e8a6ea879554
Zink Pavel <tsynk.p@gmail.com>
2022-09-19 20:57:57 +0700

    Add utils.download_file_with_message test

---
+++ unittests/test_utils.py
@@ -0,0 +1,34 @@
+import os
+import unittest
+import tempfile
+import shutil
+
+from src import utils
+
+
+class DownlaodFileWithMessageTests(unittest.TestCase):
+    def setUp(self) -> None:
+        self.tmpdir = tempfile.mkdtemp()
+
+    def tearDown(self) -> None:
+        shutil.rmtree(self.tmpdir)
+
+    def test_download_file_with_message(self):
+        python_logo_url = r"https://www.python.org/static/img/python-logo.png"
+
+        result = utils.download_file_with_message(
+            url=python_logo_url,
+            out=self.tmpdir,
+            msg="test message",
+        )
+
+        self.assertEqual(
+            os.path.normpath(
+                os.path.join(self.tmpdir, os.path.basename(python_logo_url))
+            ),
+            os.path.normpath(result),
+        )
+
+
+if __name__ == "__main__":
+    unittest.main()
