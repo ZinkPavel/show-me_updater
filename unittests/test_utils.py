272a039f52ebf0a040b775836f08d33ef08c7451
Pavel Zink <tsynk.p@gmail.com>
2022-09-29 22:23:05 +0700

    Add unitest configuration for VS Code

--- unittests/test_utils.py
+++ unittests/test_utils.py
@@ -13,13 +13,29 @@ class DownlaodFileWithMessageTests(unittest.TestCase):
     def tearDown(self) -> None:
         shutil.rmtree(self.tmpdir)
 
-    def test_download_file_with_message(self):
+    def test_base(self):
         python_logo_url = r"https://www.python.org/static/img/python-logo.png"
 
         result = utils.download_file_with_message(
             url=python_logo_url,
             out=self.tmpdir,
             msg="test message",
+        )
+
+        self.assertEqual(
+            os.path.normpath(
+                os.path.join(self.tmpdir, os.path.basename(python_logo_url))
+            ),
+            os.path.normpath(result),
+        )
+
+    def test_without_msg(self):
+        python_logo_url = r"https://www.python.org/static/img/python-logo.png"
+
+        result = utils.download_file_with_message(
+            url=python_logo_url,
+            out=self.tmpdir,
+            msg=None,
         )
 
         self.assertEqual(
